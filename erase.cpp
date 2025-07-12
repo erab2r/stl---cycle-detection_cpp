2383e443bc0f87b7bfb31bd508c50a7b5cd0cb56
erab2r <shkera213@gmail.com>
2025-07-13 01:17:31 +0600

    ss

--- erase.cpp
+++ erase.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main(){
     list<int>l ={1100,2002,3004,60,49,78,78,67};
-    l.erase(next(l.begin(),4));
+    // l.erase(next(l.begin(),4));
+     l.erase(next(l.begin(),2),next(l.begin(),6));
    for(int val : l){
     cout <<val <<endl;
    }
