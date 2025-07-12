26d53e16d66416101050647bd592a2358d013f6e
erab2r <shkera213@gmail.com>
2025-07-13 01:00:56 +0600

    2

--- modifier2.cpp
+++ modifier2.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 int main(){
     list<int>l ={1100,2002,3004};
-    l.insert(next(l.begin(),2),200);
+    list<int>l2 ={3300,44002,7004};
+
+    // l.insert(next(l.begin(),2),200);
+    l.insert(next(l.begin(),2),l2.begin(),l2.end());
    for(int val : l){
     cout <<val <<endl;
    }
