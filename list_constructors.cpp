4ffb194141f819b287755ed8443a5acfe4c78697
erab2r <shkera213@gmail.com>
2025-07-12 18:02:50 +0600

    first commit

---
+++ list_constructors.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    list<int>l(10,3);
+    // for(auto it = l.begin();it!=l.end();it++){
+    //     cout << *it << endl;
+    // }
+    for(int val:l){
+        cout << val <<endl;
+    }
+}
